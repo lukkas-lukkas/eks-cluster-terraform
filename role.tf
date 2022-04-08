d3c080fbc8d91c8ee25efee5225f8164c98bd7fe
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-04-08 20:52:52 -0300

    fix role

--- role.tf
+++ role.tf
@@ -1,23 +1,21 @@
 resource "aws_iam_role" "resourse_role" {
   name = "${var.project_name}_role"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = jsonencode({
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+    "Version": "2012-10-17"
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "resource_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/resourcePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/resource_policy"
   role       = aws_iam_role.resourse_role.name
 }
