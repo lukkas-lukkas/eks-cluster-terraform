34b54c1c313bb41cd3459c02e0354fa72d5bbe3f
Lucas Lima <66659328+lukkas-lukkas@users.noreply.github.com>
2022-04-07 21:51:18 -0300

    add ec2 instance

--- main.tf
+++ main.tf
@@ -2,17 +2,36 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
+      version = "3.26.0"
     }
   }
+
+  required_version = ">= 1.1.0"
 
   cloud {
     organization = "lukkas-lukkas"
 
     workspaces {
       name = "projectX"
+    }
+  }
+
+  provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+  }
+
+  resource "aws_instance" "app_server" {
+    ami             = "ami-0892d3c7ee96c0bf7"
+    instance_type   = "t2.micro"
+    user_data       = <<-EOF
+          #!/bin/bash
+          cd /home/ubuntu
+          echo "<h1>Ola mundo. Feito com terraform<h1>" > index.html
+          nohup busybox httpd -f -p 8080 &
+    EOF
+    tags = {
+      Name = "Instancia com arquivos"
     }
   }
 }
