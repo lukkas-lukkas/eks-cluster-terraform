223fe60afff69a261a3b2b8dc95a4de48515bb06
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-04-10 16:30:55 -0300

    add repository

--- repositories.tf
+++ repositories.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "projectx_ms_store_repository_resource" {
   name                 = "project-x-ms-store"
 }
+
+resource "aws_ecr_repository" "projectx_ms_fraud_repository_resource" {
+  name                 = "project-x-ms-fraud"
+}
